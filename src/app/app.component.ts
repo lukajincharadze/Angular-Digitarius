08e1e90a74ae8d62a0ba51f08749e746906892f4
Luka Jincharadze <lukajinchara@gmail.com>
2024-06-26 16:12:11 +0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { Section1Component } from './section1/section1.component';
+import { Section2Component } from './section2/section2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavComponent,
+    HeaderComponent,
+    Section1Component,
+    Section2Component,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'signify';
